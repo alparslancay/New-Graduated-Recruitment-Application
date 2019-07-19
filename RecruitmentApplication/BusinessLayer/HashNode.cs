2b1f67ac6222abf68816ee7a52ba57be5a38bbe8
Neslihan Kaya <neslihn.3897@gmail.com>
2019-07-19 12:38:11 +0300

    Hash Sınıfı ve HashNode Sınıfı oluşturuldu.Neslihan Kaya

---
+++ RecruitmentApplication/BusinessLayer/HashNode.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Information_Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class HashNode
+    {
+        public HashNode NextValue { get; set; }
+
+        public StudentInformation Student { get; set; }
+
+        public HashNode(StudentInformation studentInformation)
+        {
+            this.Student = studentInformation;
+        }
+    }
+}
