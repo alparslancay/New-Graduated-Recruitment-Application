6eddbbf44bd8b9cfd234e60441d78e99b326e139
alparslancay <alparslancay1@gmail.com>
2019-07-20 21:11:37 +0300

    Added FactoryOperations

---
+++ RecruitmentApplication/BusinessLayer/FactoryOperations.cs
@@ -0,0 +1,32 @@
+﻿using EntityLayer.Member_Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class FactoryOperations
+    {
+        ISystemManager GetSystemManagerOperations()
+        {
+            return new SystemManager();
+        }
+
+        ICompanyOperations GetCompanyOperations(CompanyManager companyManager)
+        {
+            return new CompanyOperations(companyManager);
+        }
+
+        IStudentOperations GetStudentOperations(MemberStudent memberStudent)
+        {
+            return new StudentOperations(memberStudent);
+        }
+
+        ILoginController GetLoginController()
+        {
+            return new LoginController();
+        }
+    }
+}
